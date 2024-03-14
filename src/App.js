c35c1101c82a81a01deb5585d7407ea87242e742
Nancylovesdata <nancyeofreeman@gmail.com>
2024-03-14 17:46:05 +0000

    Styled the top rated page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-// import './App.css';
-// import Homepage from './pages/homepage';
+import './App.css';
+import Homepage from './pages/homepage';
 
-// function App() {
-//   return (
-//     <div>
-//  <Homepage/>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import "./App.css";
-import Homepage from "./pages/homepage";
-import Toprated from "./pages/top-rated";
-
-const router = createBrowserRouter([
-  { path: "/", element: <Homepage /> },
-  { path: "/top-rated", element: <Toprated /> },
-]);
 
 function App() {
-
   return (
-    <RouterProvider router={router} />
+    <div>
+ <Homepage/>
+    </div>
   );
 }
 
 export default App;
+
--- src/pages/top-rated.jsx
+++ src/pages/top-rated.jsx
@@ -1,79 +1,24 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 
-const Modal = ({ isOpen, onClose, children }) => {
-  const [isOpenState, setIsOpenState] = useState(isOpen);
-
-  // Function to close the modal
-  const handleClose = () => {
-    setIsOpenState(false);
-    onClose();
-  };
-
-  // If modal is not open, return null
-  if (!isOpenState) return null;
-
-  return (
-    <div className="fixed z-10 inset-0 overflow-y-auto">
-      <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-        {/* Background overlay */}
-        <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-        </div>
-
-        {/* Modal content */}
-        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-scroll shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-5xl sm:w-full">
-          <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-            {/* Close button */}
-            <div className="absolute top-0 right-0 pt-4 pr-4">
-              <button
-                onClick={handleClose}
-                className="text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500 transition ease-in-out duration-150"
-              >
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-
-            {/* Modal content */}
-            {children}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
 const Toprated = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedMovieId, setSelectedMovieId] = useState(null);
-  const [selectedMovie, setSelectedMovie] = useState({});
   const [movies, setMovies] = useState(null);
-  const url =
-    "https://api.themoviedb.org/3/movie/top_rated";
-
-
+  const url = "https://api.themoviedb.org/3/movie/top_rated";
 
   useEffect(() => {
     async function displayAllMovies() {
       try {
-        const response = await axios.get(url, { headers: {
-          Authorization  : `Bearer ${process.env.REACT_APP_ACCESS_TOKEN_API_KEY}`
-        }});
+        const response = await axios.get(url, {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN_API_KEY}`
+          }
+        });
         const responseJson = response.data.results; // Access the 'results' property
-        console.log("response", responseJson);
-        setMovies(responseJson);
+        const modifiedMovies = responseJson.map((movie) => ({
+          ...movie,
+          release_year: movie.release_date.substring(0, 4) // Extracting only the year
+        }));
+        setMovies(modifiedMovies);
       } catch (error) {
         console.log(error);
       }
@@ -81,82 +26,51 @@ const Toprated = () => {
     displayAllMovies();
   }, []);
 
+  const handleRateButtonClick = (average) => {
+    alert(`The average rating is ${average}`);
+  };
 
-  if(!movies){
-    return <h1>Loading</h1>
-  
+  if (!movies) {
+    return <h1>Loading</h1>;
   }
 
-  const handleOpenModal = (id) => {
-    setSelectedMovieId(id);
-    setIsOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsOpen(false);
-  };
   return (
-    <div className="px-8 mx-auto flex flex-col gap-10">
-      <section>
-        <h1 className="text-2xl font-bold">Top Rated</h1>
-      </section>
+    <>
+      <div className="px-8 mx-auto flex flex-col gap-10">
+        <section>
+          <h1 className="text-2xl font-bold">Top Rated</h1>
+        </section>
 
-      <section className="flex flex-row gap-5 flex-wrap justify-center">
-
-        
-        {/* Movie card */}
-        <div className="shadow-md" onClick={() => handleOpenModal(1)}>
-          <img
-            src="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-            alt="avengers"
-            width="300px"
-            height="500px"
-          />
-
-          <div className="flex flex-col text-center p-3 relative">
-            <h2>
-              <span className="text-xl font-semibold">Avengers</span> (2012)
-            </h2>
-            <p className="text-xs font-light">02 hours 30 minutes</p>
-            {/* <div className="bg-orange-500 rounded-full w-12 h-12 mx-auto flex items-center mt-2 absolute top-[-30px] right-2"> */}
-            <div className="bg-orange-500 rounded-full w-12 h-12 mx-auto flex items-center mt-2">
-              <p className="p-1 ps-2 text-white">80%</p>
-            </div>
-          </div>
-        </div>
-
-        {isOpen && (
-          <Modal isOpen={isOpen} onClose={handleCloseModal}>
-            {/* Modal content */}
-            <div className="flex flex-row gap-5">
-              <img
-                src="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
-                alt="Avengers"
-                width="400px"
-                height="700px"
-              />
-              <div className="flex flex-col">
-                <h2 className="text-3xl">
-                  <span className="font-semibold">Avengers</span>
-                  (2012)
+        <section className="grid grid-cols-5 gap-5 justify-center">
+          {movies.map((item) => (
+            <div key={item.id} className="shadow-md relative">
+              <div className="text-center relative">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                  alt={item.title}
+                  width="250px"
+                  height="350px"
+                  style={{borderRadius:"10px"}}
+                />
+                <button
+                  className="bg-orange-500 rounded-md px-1 py-1 text-white absolute bottom-0 left-0 right-0 mx-auto mb-1 text-xs"
+                  style={{ width: "80px" }}
+                  onClick={() => handleRateButtonClick(item.vote_average)}
+                >
+                  RATING: {item.vote_average.toFixed(1)}
+                </button>
+              </div>
+              <div className="text-center">
+                <h2 className="text-md font-semibold text-ellipsis mb-1">
+                  {item.title}
                 </h2>
-
-                <div className="flex flex-row mt-2 items-center">
-                  <span className="text-xs border border-1 border-grey-400 px-2 py-1 rounded-full">
-                    Action
-                  </span>
-                  &nbsp;
-                  <span className="text-xs border border-1 border-grey-400 px-2 py-1 rounded-full">
-                    Sci-Fi
-                  </span>
-                  <p className="text-xs font-light ms-3">02 hours 30 minutes</p>
-                </div>
+                <p>{item.release_year}</p>
               </div>
             </div>
-          </Modal>
-        )}
-      </section>
-    </div>
+          ))}
+        </section>
+      </div>
+    </>
   );
 };
 

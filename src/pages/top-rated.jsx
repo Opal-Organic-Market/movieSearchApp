2d6ce17211256993d888f7cdc0349a2cbd538069
JoanaQuadzie <joanaq03@gmail.com>
2024-03-15 12:50:03 +0000

    fixed rating card

--- src/pages/top-rated.jsx
+++ src/pages/top-rated.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+
 const Toprated = () => {
   const [movies, setMovies] = useState(null);
   const url = "https://api.themoviedb.org/3/movie/top_rated";
@@ -35,35 +36,31 @@ const Toprated = () => {
   }
 
   return (
-    <>
-          <div>
-            {/* add browse for more so when user clicks on it it shows all movies */}
-
+    <div>
       <div className="flex justify-center mt-9">
-        <div className="container"> <span className="font-extrabold	">Top-Rated</span>
-          <div className=" flex overflow-auto gap-x-2  ">
+        <div className="container">
+          <span className="font-extrabold">Top-Rated</span>
+          <div className="flex overflow-auto gap-x-2">
             {movies.map((item, index) => (
-              <div key={index} className="flex-shrink-0 ">
+              <div key={index} className="flex-shrink-0">
                 <Link to={'/moviedetailspage/' + item.id}>
-                <div className=" w-48 h-auto p-4 rounded-md ">
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
-                    alt="movies"
-                    className="h-64 rounded-md "
-                  />
-                  
-                  <h2 className="mt-2 text-base font-semibold truncate h-7" >
-                  {/* <button
-                  className="bg-orange-500 rounded-md px-1 py-1 text-white absolute bottom-0 left-0 right-0 mx-auto mb-1 text-xs"
-                  style={{ width: "80px" }}
-                  onClick={() => handleRateButtonClick(item.vote_average)}
-                >
-                  RATING: {item.vote_average.toFixed(1)}
-                </button>
-                    */}
-                    {item.original_title} 
-                  </h2>
-                </div>
+                  <div className="w-48 h-auto p-4 rounded-md">
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                      alt="movies"
+                      className="h-64 rounded-md"
+                    />
+                    <button
+                      className="bg-orange-500 rounded-md px-1 py-1 text-white bottom-0 left-0 right-0 mx-auto mb-1 text-xs"
+                      style={{ width: "80px" }}
+                      onClick={() => handleRateButtonClick(item.vote_average)}
+                    >
+                      RATING: {item.vote_average.toFixed(1)}
+                    </button>
+                    <h2 className="mt-2 text-base font-semibold truncate h-7">
+                      {item.original_title}
+                    </h2>
+                  </div>
                 </Link>
               </div>
             ))}
@@ -71,7 +68,6 @@ const Toprated = () => {
         </div>
       </div>
     </div>
-    </>
   );
 };
 
